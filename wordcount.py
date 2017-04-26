e8c0eae881e522052b8fc1c3b530e828edc7cf71
Salvatore Avena <sja33@pitt.edu>
2017-04-26 17:09:12 -0400

    better UI, almost working

--- wordcount.py
+++ wordcount.py
@@ -1,18 +1,13 @@
 from __future__ import print_function
 
-import os
-import sys
-import math
-import collections
-from collections import OrderedDict
 import re
-from operator import add
-
+import sys
+import json
 import string
-from string import digits
-from string import punctuation
-from string import maketrans
 
+from string import digits
+from string import maketrans
+from string import punctuation
 from pyspark import SparkContext
 
 def cleanFile (name):
@@ -22,91 +17,37 @@ def cleanFile (name):
     return name
 
 def cleanWord (word):
-    word= re.sub('[^a-zA-Z]+', '', word.lower())
+    word = re.sub('[^a-zA-Z]+', '', word.lower())
     return word
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: wordcount <file>", file=sys.stderr)
-        exit(-1)
     sc = SparkContext(appName="PythonWordCount")
     line = sc.textFile("file:///mounted_volume/books/*.txt")
     lines = sc.textFile(sys.argv[1], 1)
-    # counts = lines.flatMap(lambda x: x.split(' ')) \
-    #               .map(lambda x: (x, 1)) \
-    #               .reduceByKey(add)
-
-    # counts = sc.wholeTextFiles(sys.argv[1], 1)\
-	# 	.flatMap(lambda (name, content): map(lambda word: (word, name), content.split(' ')))\
-	# 	.map(lambda (word, name): ((word, name), 1))\
-	# 	.reduceByKey(lambda count1, count2: count1 + count2)\
-    #   .map(lambda ((word, name), count): (word, name, count))
 
     counts = sc.wholeTextFiles(sys.argv[1], 1)\
-		.flatMap(lambda (name, content): map(lambda word: (word, name), content.split(' ')))\
-		.map(lambda (word, name): ((cleanWord(word), cleanFile (name)), 1))\
-		.reduceByKey(lambda count1, count2: count1 + count2)\
+        .flatMap(lambda (name, content): map(lambda word: (word, name), content.split(' ')))\
+        .map(lambda (word, name): ((cleanWord(word), cleanFile (name)), 1))\
+        .reduceByKey(lambda count1, count2: count1 + count2)\
         .map(lambda ((word, name), count): (word, name, count))
-
     output = counts.collect()
+
     ii = {}
     fileList =[]
+    indexFile = './inverted-index.json'
 
     for (word, name, count) in output:
-
         if word not in ii:
-            #print("Added %s to ii with word %s and count %i" % (name.encode("utf-8"), word.encode("utf-8"), count))
             ii[word] = {}
             ii[word][name] = count
         else:
-            #print("updated count for: %s to ii with word %s and to new count %i" % (name.encode("utf-8"), word.encode("utf-8"), count))
             ii[word][name] = count
-
         if name not in fileList:
             fileList.append(name)
 
-    numFiles = len(fileList)
-    Results = {}
-    search = sys.argv[2]
-    terms = search.split()
+    ii['fileList']=fileList
 
-    # for testWord in ii.keys():
-    #     print('Word: #%s# ' % testWord.encode("utf-8"))
-    #     for testDoc in ii[testWord].keys():
-    #         print('\tDoc: '+testDoc.encode("utf-8"))
-    #         print('\t\tCount: '+ str(ii[testWord][testDoc]))
+    with open(indexFile, "w+") as g:                                                  #save dict
+        json.dump(ii, g)
 
-
-    for term in terms:                                                            #loop through keywords
-        if term not in Results:                                                          #Only calculate if not already done
-            if term in ii:                                                                 # Only calculate if keyword in any file
-                Results[term] = {}                                                               #Create Results dict
-                for fileName in fileList:                                                           #Loop through files
-                    freq = 0                                                                            #initializ freq to zero
-                    try:                                                                                #handle exceptions where keyword isnt in file
-                        freq = ii[term][fileName]                                                      #calculate frequency of keyword in file
-                    except KeyError:                                                                    # handle exceptions where keyword isnt in file
-                        Results[term][fileName] = 0.000000                                           # handle exceptions where keyword isnt in file
-                    if (freq > 0):                                                                      #calculate TF
-                        tf = 1 + math.log(freq, 2)                                                          # as 1+log2(freq) or 0
-                    else:                                                                                   # depending on
-                        tf = 0                                                                              # Frequency
-                    IDF = math.log((float(numFiles) / len(ii[term])), 2)                           #calculate IDF = log2(N/n)
-                    weight = tf * IDF                                                                   #calculate weight = TF * IDF
-                    # print (weight)
-                    Results[term][fileName] = weight                                                 #add weight for keyword, file
-            else:                                                                               #Handle cases where
-                for fileName in fileList:                                                           # keyword isnt in
-                    if term not in Results:                                                          # any file
-                        Results[term] = {fileName : 0.000000}                                             # if first time
-                    else:                                                                               #
-                        Results[term][fileName] = 0.000000                                               # if already in results for some file
-
-    for searchTerm in Results.keys():
-        termResults = Results[term]
-        sortedTermResults = sorted(termResults, key=lambda x: termResults.values())
-        print (searchTerm + "_____________________________")
-        for doc in termResults.keys():
-            print ("----Document: "+ doc)
-            print ("------------Weight: "+str(termResults[doc]))
     sc.stop()
---
+++ working.py
@@ -0,0 +1,89 @@
+from __future__ import print_function
+
+import re
+import os
+import sys
+import json
+import math
+import string
+import os.path
+import collections
+
+from operator import add
+from string import digits
+from string import maketrans
+from string import punctuation
+from collections import OrderedDict
+
+quit = False
+
+def cleanWord (word):
+    word = re.sub('[^a-zA-Z]+', '', word.lower())
+    return word
+
+def search (terms):
+    global quit
+    for term in terms:                                                            #loop through keywords
+        if term == ':q':
+            quit = True
+            break
+        term = cleanWord(term)
+        if term not in Results:                                                          #Only calculate if not already done
+            if term in ii:                                                                 # Only calculate if keyword in any file
+                Results[term] = {}                                                               #Create Results dict
+                for fileName in fileList:                                                           #Loop through files
+                    freq = 0                                                                            #initializ freq to zero
+                    try:                                                                                #handle exceptions where keyword isnt in file
+                        freq = ii[term][fileName]                                                      #calculate frequency of keyword in file
+                    except KeyError:                                                                    # handle exceptions where keyword isnt in file
+                        Results[term][fileName] = 0.000000                                           # handle exceptions where keyword isnt in file
+                        print("KEY ERROR, assigning weight=0")
+                    if (freq > 0):                                                                      #calculate TF
+                        tf = 1 + math.log(freq, 2)                                                          # as 1+log2(freq) or 0
+                    else:                                                                                   # depending on
+                        tf = 0                                                                              # Frequency
+
+                    print("FREQ IS: "+(str)freq)
+                    IDF = math.log((float(numFiles) / len(ii[term])), 2)                           #calculate IDF = log2(N/n)
+                    print("IDF IS: "+(str)IDF)
+                    weight = tf * IDF                                                                   #calculate weight = TF * IDF
+                    print("WEIGHT IS: "+(str)weight)
+                    Results[term][fileName] = weight                                                 #add weight for keyword, file
+            else:                                                                               #Handle cases where
+                print("____________IS NOT IN II")
+                for fileName in fileList:                                                           # keyword isnt in
+                    if term not in Results:                                                          # any file
+                        Results[term] = {fileName : 0.000000}                                             # if first time
+                    else:                                                                               #
+                        Results[term][fileName] = 0.000000                                               # if already in results for some file
+
+    for searchTerm in Results.keys():
+        termResults = Results[term]
+        sortedTermResults = sorted(termResults, key=lambda x: termResults.values())
+        print (searchTerm + "_____________________________")
+        for doc in termResults.keys():
+            print ("----Document: "+ doc)
+            print ("------------Weight: "+str(termResults[doc]))
+
+if __name__ == "__main__":
+    indexFile = './inverted-index.json'
+
+    ii = {}
+    fileList =[]
+    if os.path.isfile(indexFile):
+        print("Using existing index")
+    else:
+        print("ABOUT TO BUILD INDEX")
+        os.system('spark-submit file:///mounted_volume/wordcount.py file:///mounted_volume/books/*.txt')
+
+    with open(indexFile, "r+") as f:
+        ii = json.load(f)
+
+    fileList = ii['fileList']
+    numFiles = len(fileList)
+
+    while not quit:
+        Results = {}
+        searchTerms = raw_input("Enter your search terms (or ':q' to quit): ")
+        terms = searchTerms.split()
+        search(terms)
